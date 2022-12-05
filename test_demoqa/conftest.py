b609e8462fda513cabf2316bfb90c8bda0c81a99
DmanYV <dima-xxx@mail.ru>
2022-12-05 12:19:10 +0300

    +add tests

--- test_demoqa/conftest.py
+++ test_demoqa/conftest.py
@@ -5,9 +5,9 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_managment():
-    browser.config.base_url('https://demoqa.com/automation-practice-form')
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.timeout = 3.5
+    browser.config.timeout = 3.0
     yield
     browser.quit()
--- test_demoqa/test_demoqa_form.py
+++ test_demoqa/test_demoqa_form.py
@@ -1,10 +1,9 @@
-import pytest
 from selene.support.shared import browser
-from selene import be, have
+from selene import have
 
 
 def test_form():
-    browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
+    browser.open('/automation-practice-form')
     browser.element('#firstName').type('Dmitry')
     browser.element('#lastName').type('Yanyshev')
     browser.element('#userEmail').type('mail@mail.ru')
@@ -12,3 +11,13 @@ def test_form():
     browser.element('#userNumber').type('1234567891')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
+    browser.element('[value="3"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('[value="1995"]').click()
+    browser.element('.react-datepicker__day--006').click()
+    browser.element('#subjectsInput').type('Maths').press_enter()
+    browser.element('#hobbiesWrapper').element('#hobbies-checkbox-1').double_click()
+
+
+
+
